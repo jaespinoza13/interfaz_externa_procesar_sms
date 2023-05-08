4b6f4d176273175ba2dbab279d3bd39e062e1494
pdarevalo <pdarevalo@mego.net>
2023-05-08 16:52:46 -0500

    Eliminar comentarios innecesarios

--- InterfazExternaProcesarSms/Program.cs
+++ InterfazExternaProcesarSms/Program.cs
@@ -5,10 +5,8 @@ using InterfazExternaProcesarSms.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 var allowSpecificOrigins = "_AllowSpecifiOrigins";
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(s =>
 {
@@ -66,7 +64,6 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
