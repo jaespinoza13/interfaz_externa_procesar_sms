fd1ee14554c91b0ee5623c5e0315d061e7beb5d1
pdarevalo <pdarevalo@mego.net>
2023-05-08 17:20:35 -0500

    Configure the HTTP request pipeline comment

--- InterfazExternaProcesarSms/Program.cs
+++ InterfazExternaProcesarSms/Program.cs
@@ -5,8 +5,10 @@ using InterfazExternaProcesarSms.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 var allowSpecificOrigins = "_AllowSpecifiOrigins";
 
-builder.Services.AddControllers();
+// Add services to the container.
 
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(s =>
 {
@@ -64,6 +66,7 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
