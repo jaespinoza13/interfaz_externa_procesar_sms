102bd862dfa83e7edf3b9a1aaf5dfccb2d62ec9d
jespinoza <jespinoza@mego.net>
2023-07-25 08:56:09 -0500

    Test

--- InterfazExternaProcesarSms/Program.cs
+++ InterfazExternaProcesarSms/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddSwaggerGen(s =>
         Name = "Authorization",
         Type = SecuritySchemeType.Http,
         Scheme = "Basic",
-        In = ParameterLocation.Header,
+        In = ParameterLocation.Header, 
         Description = "Seguridad básica del API"
     });
 
